1bbea27169ea2abc2549eeef36785f4a12ed9fd6
Mikael Alves <JOSE.ALVES08240@alunos.ufersa.edu.br>
2026-02-21 11:02:34 -0300

    fix: request blocking in production

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.setGlobalPrefix('api');
   await app.listen(3000);
